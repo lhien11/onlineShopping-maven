7b9226278ffd3fa6594818e068067eea3b9b6aa5
hienle <lhien11@gmail.com>
2018-02-01 03:37:34 -0800

    initial commit

---
+++ shoppingbackend-maven/src/main/java/com/lhien11/shoppingbackend_maven/App.java
@@ -0,0 +1,13 @@
+package com.lhien11.shoppingbackend_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
